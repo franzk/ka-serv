f720f59172be48047a964c44ad92af64d5bc1a8f
François Koehler <francois.koehler@lde.fr>
2025-05-22 23:06:04 +0200

    smtp-gateway & bruno

--- ka-mailer/src/main/java/net/franzka/kaserv/ka_mailer/controller/MailController.java
+++ ka-mailer/src/main/java/net/franzka/kaserv/ka_mailer/controller/MailController.java
@@ -3,6 +3,7 @@ package net.franzka.kaserv.ka_mailer.controller;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.franzka.kaserv.ka_mailer.dto.MailRequest;
 import net.franzka.kaserv.ka_mailer.service.MailService;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/mail")
 @RequiredArgsConstructor
-public class MailController {
+@Slf4j
+public class
+MailController {
 
     private final MailService mailService;
 
     @PostMapping
-    // @PreAuthorize("hasAuthority('ROLE_mailer:send')")
+    @PreAuthorize("hasRole('ROLE_mailer:send')")
     public ResponseEntity<String> sendMail(@RequestBody @Valid MailRequest mailRequest) throws MessagingException {
+        log.info("sendMail : {}", mailRequest);
         mailService.sendMail(mailRequest);
         return ResponseEntity.ok("Mail sent");
     }
