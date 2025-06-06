5fcc407573690a15e90eb64b06dea1df64272683
François Koehler <francois.koehler@lde.fr>
2025-06-07 01:15:05 +0200

    correction

--- keycloak/start.sh
+++ keycloak/start.sh
@@ -5,11 +5,26 @@ echo "=== Entrée dans start.sh ==="
 
 # Affichage debug des variables
 echo "KEYCLOAK_HOSTNAME=${KEYCLOAK_HOSTNAME}"
+echo "KEYCLOAK_ADMIN=${KEYCLOAK_ADMIN}"
+echo "KEYCLOAK_ADMIN_PASSWORD=${KEYCLOAK_ADMIN_PASSWORD}"
 
 # Valeurs par défaut si non définies
+: "${KEYCLOAK_ADMIN:=admin}"
+: "${KEYCLOAK_ADMIN_PASSWORD:=admin}"
 : "${KEYCLOAK_HOSTNAME:=localhost}"
 
-exec /opt/keycloak/bin/kc.sh start --optimized --import-realm \
+# Création de l'admin si base vide
+if [ ! -f /opt/keycloak/data/h2/keycloak.mv.db ] && [ ! -f /opt/keycloak/data/import-done.flag ]; then
+  echo "🛠️  Bootstrapping admin user..."
+  /opt/keycloak/bin/kc.sh bootstrap-admin user \
+    --user "$KEYCLOAK_ADMIN" \
+    --password "$KEYCLOAK_ADMIN_PASSWORD"
+  touch /opt/keycloak/data/import-done.flag
+fi
+
+exec /opt/keycloak/bin/kc.sh start \
+  --optimized \
+  --import-realm \
   --http-enabled=true \
   --http-port=8080 \
   --proxy-headers=xforwarded \
