81f6d25c18717ae1aff5c01753944eb0efa39601
François Koehler <francois.koehler@lde.fr>
2025-06-12 00:47:25 +0200

    correction

--- ka-mailer/src/main/java/net/franzka/kaserv/ka_mailer/service/MailService.java
+++ ka-mailer/src/main/java/net/franzka/kaserv/ka_mailer/service/MailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.franzka.kaserv.ka_mailer.dto.MailRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,14 @@ public class MailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String defaultFromAddress;
+
     public void sendMail(MailRequest request) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        helper.setFrom(request.getFrom());
+        helper.setFrom(defaultFromAddress);
         helper.setTo(request.getTo());
         helper.setSubject(request.getSubject());
 
@@ -30,7 +34,7 @@ public class MailService {
             helper.setText(request.getText(), false);
         }
 
-        log.info("Sending mail from {} to {} with subject {}", request.getFrom(), request.getTo(), request.getSubject());
+        log.info("Sending mail from {} to {} with subject {}", defaultFromAddress, request.getTo(), request.getSubject());
         mailSender.send(message);
     }
 
