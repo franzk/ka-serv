90efb09a037f6b10f047350f82b1502b5021f27e
franzk <koehler.francois@gmail.com>
2025-06-13 00:10:38 +0200

    correction

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "✍️ Generating .env.prod for ka-smtp-gateway..."
 ./ka-smtp-gateway/scripts/set-keycloak-client-secret.sh \
   --secret "$KA_SMTP_GATEWAY_SECRET"
 
-# echo "🔐 Injecting secret into Keycloak..."
+echo "🔐 Injecting secret into Keycloak..."
 ./keycloak/scripts/patch-realm-client-secret.sh \
   --client-id "ka-smtp-gateway-client" \
   --client-secret "$KA_SMTP_GATEWAY_SECRET"
