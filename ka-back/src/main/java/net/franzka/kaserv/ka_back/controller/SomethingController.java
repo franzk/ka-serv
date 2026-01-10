da608720ddf60a5b7a50f85ea2f1e1babee14f42
Franz Ka <koehler.francois@gmail.com>
2026-01-10 23:38:46 +0100

    sécurité

--- ka-back/src/main/java/net/franzka/kaserv/ka_back/controller/SomethingController.java
+++ ka-back/src/main/java/net/franzka/kaserv/ka_back/controller/SomethingController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.franzka.kaserv.ka_back.domain.dto.SomethingDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,6 @@ import java.util.Date;
 public class SomethingController {
 
     @GetMapping("/something")
-    @PreAuthorize("hasRole('user')")
     public ResponseEntity<SomethingDto> something() {
         SomethingDto somethingDto = SomethingDto.builder()
                 .date(new Date())
