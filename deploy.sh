822c71ec0cd5c755c32bcdb154768d569b56e9ba
Franz Ka <koehler.francois@gmail.com>
2026-01-12 22:55:25 +0100

    amélioration env de prod et deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 echo "🚀 Starting Docker Compose..."
-docker compose up -d --build
+docker compose -f docker-compose.yml -f docker-compose.proxy.yml up -d
 
 echo "✅ Deployment complete."
