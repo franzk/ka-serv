7f7c78c6d3249e483f100fcf4ee865aecaf90b66
Franz Ka <koehler.francois@gmail.com>
2026-01-17 01:56:13 +0100

    doc et nettoyage

--- ka-gateway/src/main/java/net/franzka/kaserv/ka_gateway/configuration/SecurityConfig.java
+++ ka-gateway/src/main/java/net/franzka/kaserv/ka_gateway/configuration/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().denyAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
