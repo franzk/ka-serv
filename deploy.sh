2d78b5950aff4e5c5ed92910bc280106b2538701
Franz Ka <koehler.francois@gmail.com>
2026-01-17 23:17:08 +0100

    améliore deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 echo "🚀 Starting Docker Compose..."
-docker compose -f docker-compose.yml -f docker-compose.proxy.yml up -d
+docker compose -f docker-compose.yml -f docker-compose.proxy.yml up --build --force-recreate -d
 
 echo "✅ Deployment complete."
